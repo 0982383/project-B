848e0ee0cd9cf62ad60861e85b6c5d342ae61c66
0982383 <60737703+0982383@users.noreply.github.com>
2020-02-11 11:33:30 +0100

    Add project files.

---
+++ project B/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace project_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Issie");
+            Console.ReadKey();
+        }
+    }
+}
