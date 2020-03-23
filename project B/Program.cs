29b5d0eb061b48e824ae2984e28bd24197c979ab
0982383 <60737703+0982383@users.noreply.github.com>
2020-03-23 16:45:08 +0100

    Update Program.cs

--- project B/Program.cs
+++ project B/Program.cs
@@ -13,7 +13,10 @@ namespace project_B
             Console.WriteLine("Feuzi");
             Console.WriteLine(" Yo dit is een test , 23-2-2020");
             Console.WriteLine(" nog een test , 10-3-2020");
-            Console.ReadKey();
+
+
+           
+
         }
     }
 }
