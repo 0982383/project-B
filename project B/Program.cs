885c7b48624675d7fb62334dbd2136eb27e8439e
0982383 <60737703+0982383@users.noreply.github.com>
2020-03-23 16:54:29 +0100

    Update Program.cs

--- project B/Program.cs
+++ project B/Program.cs
@@ -2,17 +2,53 @@
 
 namespace project_B
 {
+    public class UserInfo
+    {
+        public string Username;
+        public string Password;
+
+        public void Task1()
+        {
+            Console.WriteLine("Enter Username:");
+
+        }
+        public void Task2()
+        {
+            Console.WriteLine("Enter Password:");
+        }
+
+        public void Succes()
+        {
+            Console.WriteLine("Welcome Back " + Username);
+        }
+    }
+
+    public class Customer
+    {
+        public void Guest()
+        {
+            Console.WriteLine("Guest");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Ismaïl, test");
-            Console.WriteLine("WHATSUP HOMIES, Mr.PATRIQUE IN DA HOUSE!");
-            Console.WriteLine("Jullie zijn noob , groetjes Tom");
-            Console.WriteLine("Feuzi");
-            Console.WriteLine(" Yo dit is een test , 23-2-2020");
-            Console.WriteLine(" nog een test , 10-3-2020");
+        {
+
+            UserInfo x = new UserInfo();
+
+            x.Task1();
+            x.Username = Console.ReadLine();
+
+            x.Task2();
+            x.Password = Console.ReadLine();
+            x.Succes();
+
+
+
+
+
 
 
            
