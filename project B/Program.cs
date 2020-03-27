268547edc19aa71922617ec5392e7007648587dc
Patryk <0989317@hr.nl>
2020-03-27 16:51:07 +0100

    Cleaning up gedaan. Lees AUB REAM ME. Vanaf nu: Main programma wordt gemaakt in :Program.cs. Classes voeg je toe aan Classes.cs. Oude code zit in Old_Code.Cs LET OP het is allemaal geplakt als comment dus het voert nu niks uit.

--- project B/Program.cs
+++ project B/Program.cs
@@ -1,112 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
 
 namespace project_B
 {
-    public class UserInfo
-    {
-        public string Username;
-        public string Password;
-
-        public void Task1()
-        {
-            Console.WriteLine("Enter Username:");
-
-        }
-        public void Task2()
-        {
-            Console.WriteLine("Enter Password:");
-        }
-
-        public void Succes()
-        {
-            Console.WriteLine("Welcome Back " + Username);
-        }
-    }
-    public class Customer
-    {
-        public string FirstName;
-        public string LastName;
-        public string Email;
-        public int PhoneNumber;
-
-
-        public void Guest()
-        {
-            Console.WriteLine("Guest");
-        }
-    }
-    public class Movies
-    {
-        public int movieID;
-        public string MovieName;
-        public int MovieDuration;
-        public double MoviePrice;
-
-    }
-    public class Reservation
-    {
-        public int OrderID;
-        public string CustomerName;
-        public string Seats;
-        public string Movie;
-        public int Price;
-
-
-    }
-    public class Payment
-    {
-        public string PaymentMethod;
-        public string Price;
-        public int OrderID;
-
-
-    }
-    public class Seats
-    {
-        public  int Hall;
-        public  int SeatNumber;
-        public  int Row;
-    }
-    public class Hall
-    {
-        public int Seats;
-        public int Rows;
-        public string SpecialEffects;
-    }
-    public class Admin
-    {
-        public string Firstname;
-        public string Lastname;
-        public string Email;
-        public int phonenumber;
-
-        public void admin()
-        {
-            Console.WriteLine("Admin");
-        }
-    }   
     class Program
     {
+        // Entry point
         static void Main(string[] args)
         {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu()
+                    ;
+            }
+        }
+        public static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome To Our Cinema Application!");
+            Console.WriteLine("1] Log in as Customer.");
+            Console.WriteLine("2] Log in as employee.");
+            Console.WriteLine("3] Log in as manager.");
+            Console.WriteLine("4] Exit.");
+            Console.Write("\r\nSelect an option: ");
 
-            UserInfo x = new UserInfo();
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    customer();
+                    return true;
+                case "2":
+                    employee();
+                    return true;
+                case "3":
+                    manager();
+                    return true;
+                case "4":
+                    return false;
+                default:
+                    return true;
 
-            x.Task1();
-            x.Username = Console.ReadLine();
-
-            x.Task2();
-            x.Password = Console.ReadLine();
-            x.Succes();
-
-
-
-
-
-
-
-           
-
+            }
+        }
+        public static void customer()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome customer!");
+        }
+        public static void employee()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome employee!");
+        }
+        public static void manager()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome manager!");
         }
     }
 }
