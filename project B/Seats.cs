d4e39906eb3637af773150425696e394929cab68
Feuzi <60713322+Feuzi@users.noreply.github.com>
2020-03-29 23:05:50 +0200

    seats class update

--- project B/Seats.cs
+++ project B/Seats.cs
@@ -4,128 +4,96 @@ using System.Text;
 
 namespace project_B
 {
-    
-        public class Seats
+
+    public class Seats
+    {
+        public int Hall;
+        public int SeatNumber;
+        public int Row;
+
+        public void chooseSeat()
         {
-            public int Hall;
-            public int SeatNumber;
-            public int Row;
-
-            public void chooseSeat()
+            if (Hall == 1)
             {
-
-                if (Hall == 1)
+                static void seatScreen(string[] seats)
                 {
-                    static void seatScreen(int[] seats)
+                    for (int z = 1; z < seats.Length; z++)
                     {
-
-                        for (int z = 1; z < seats.Length; z++)
+                        seats[z] = "" + z;  // puts number in aray
+                        Console.Write(seats[z] + " ");  // writes seatnumber on console
+                        if (z % 10 == 0)
                         {
-                            seats[z] = z;
-
-                            Console.Write(seats[z] + " ");
-                            if (z % 10 == 0)
-                            {
-                                Console.WriteLine("\n");
-                            }
-
-
+                            Console.WriteLine("\n"); // every 10 seats new line
                         }
-
-
-                        Console.WriteLine(seats);
-                        Console.Write("Type a number from 1 to 150 to choose your seat: ");
-                        string bookedSeats = Console.ReadLine();
-                        Console.WriteLine("You have chosen seat: " + bookedSeats);
-                        int numberBookedSeats = int.Parse(bookedSeats);
-                        if (numberBookedSeats == seats[numberBookedSeats])
-                        {
-                            seats[numberBookedSeats] = 0;
-                        }
-                        Console.WriteLine(seats);
-
-
                     }
-                    seatScreen(new int[151]);
-
-
-
+                    Console.Write("Type a number from 1 to 150 to choose your seat: ");
+                    string bookedSeats = Console.ReadLine();    // bookedSeats gives chosen seatnumber
+                    int numberBookedSeats = int.Parse(bookedSeats); // convert (string bookedSeats) to (int numberBookedSeats)
+                    Console.WriteLine("You have chosen seat: " + bookedSeats);
+                    string c = seats[numberBookedSeats];    // assign string from array to string c
+                    int d = int.Parse(c);   // convert string c to int d      
+                    if (numberBookedSeats == d)
+                    {
+                        seats[d] = "*"; // replaces seat in array with *
+                    }
                 }
+                seatScreen(new string[151]);
+            }
 
-                if (Hall == 2)
+            if (Hall == 2)
+            {
+                static void seatScreen(string[] seats)
                 {
-                    static void seatScreen(int[] seats)
+                    for (int z = 1; z < seats.Length; z++)
                     {
-
-                        for (int z = 1; z < seats.Length; z++)
+                        seats[z] = "" + z;  // puts number in aray
+                        Console.Write(seats[z] + " ");  // writes seatnumber on console
+                        if (z % 10 == 0)
                         {
-                            seats[z] = z;
-
-                            Console.Write(seats[z] + " ");
-                            if (z % 10 == 0)
-                            {
-                                Console.WriteLine("\n");
-                            }
-
-
+                            Console.WriteLine("\n"); // every 10 seats new line
                         }
-
-
-                        Console.WriteLine(seats);
-                        Console.Write("Type a number from 1 to 150 to choose your seat: ");
-                        string bookedSeats = Console.ReadLine();
-                        Console.WriteLine("You have chosen seat: " + bookedSeats);
-                        int numberBookedSeats = int.Parse(bookedSeats);
-                        if (numberBookedSeats == seats[numberBookedSeats])
-                        {
-                            seats[numberBookedSeats] = 0;
-                        }
-                        Console.WriteLine(seats);
-
-
                     }
-                    seatScreen(new int[301]);
+                    Console.Write("Type a number from 1 to 150 to choose your seat: ");
+                    string bookedSeats = Console.ReadLine();    // bookedSeats gives chosen seatnumber
+                    int numberBookedSeats = int.Parse(bookedSeats); // convert (string bookedSeats) to (int numberBookedSeats)
+                    Console.WriteLine("You have chosen seat: " + bookedSeats);
+                    string c = seats[numberBookedSeats];    // assign string from array to string c
+                    int d = int.Parse(c);   // convert string c to int d      
+                    if (numberBookedSeats == d)
+                    {
+                        seats[d] = "*"; // replaces seat in array with *
+                    }
                 }
-                if (Hall == 3)
+                seatScreen(new string[301]);
+            }
+
+            if (Hall == 3)
+            {
+                static void seatScreen(string[] seats)
                 {
-                    static void seatScreen(int[] seats)
+                    for (int z = 1; z < seats.Length; z++)
                     {
-
-                        for (int z = 1; z < seats.Length; z++)
+                        seats[z] = "" + z;  // puts number in aray
+                        Console.Write(seats[z] + " ");  // writes seatnumber on console
+                        if (z % 10 == 0)
                         {
-                            seats[z] = z;
-
-                            Console.Write(seats[z] + " ");
-                            if (z % 10 == 0)
-                            {
-                                Console.WriteLine("\n");
-                            }
-
-
+                            Console.WriteLine("\n"); // every 10 seats new line
                         }
-
-
-                        Console.WriteLine(seats);
-                        Console.Write("Type a number from 1 to 150 to choose your seat: ");
-                        string bookedSeats = Console.ReadLine();
-                        Console.WriteLine("You have chosen seat: " + bookedSeats);
-                        int numberBookedSeats = int.Parse(bookedSeats);
-                        if (numberBookedSeats == seats[numberBookedSeats])
-                        {
-                            seats[numberBookedSeats] = 0;
-                        }
-                        Console.WriteLine(seats);
-
-
                     }
-                    seatScreen(new int[501]);
-
+                    Console.Write("Type a number from 1 to 150 to choose your seat: ");
+                    string bookedSeats = Console.ReadLine();    // bookedSeats gives chosen seatnumber
+                    int numberBookedSeats = int.Parse(bookedSeats); // convert (string bookedSeats) to (int numberBookedSeats)
+                    Console.WriteLine("You have chosen seat: " + bookedSeats);
+                    string c = seats[numberBookedSeats];    // assign string from array to string c
+                    int d = int.Parse(c);   // convert string c to int d      
+                    if (numberBookedSeats == d)
+                    {
+                        seats[d] = "*"; // replaces seat in array with *
+                    }
                 }
-
-
-
-
+                seatScreen(new string[501]);
             }
         }
-
     }
+
+}
