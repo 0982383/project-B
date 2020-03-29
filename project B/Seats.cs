40191826f04807669fa5d3f19c5fb9e3f9c8b7d6
Tom <60721897+Doomfist1@users.noreply.github.com>
2020-03-29 16:50:59 +0200

    Create Seats.cs

---
+++ project B/Seats.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project_B
+{
+    public class Seats
+    {
+        public int Hall;
+        public int SeatNumber;
+        public int Row;
+
+        public void chooseSeat()
+        {
+            Console.WriteLine("Choose your seat.");
+            if (Hall == 1) ;
+            {
+                string s = "";
+                for (int i = 0; i <= 150; i++)
+                {
+                    s += i + " ";
+                    if ((i == 10)) ;
+                    {
+                        s += "\r\n";
+                    }
+                }
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+  
+}
