5a91ae19c5eba580fb6e9574cb1c5c22bad2374a
0982383 <60737703+0982383@users.noreply.github.com>
2020-06-08 18:14:41 +0200

    .

--- project B/01_MainMenu.cs
+++ project B/01_MainMenu.cs
@@ -712,6 +712,11 @@ namespace Project_B
                         MovieOptions.MovieList(previousScreen, userClick);
                         break;
                     }
+                default:
+                    {
+                        ChooseSeat(userClick);
+                        break;
+                    }
                
             }
             // End User Input Oucome
@@ -937,6 +942,7 @@ namespace Project_B
                 userClick.AddInput("Choose Seat => ");
                 SeatOptions.ChooseSeat(1, MovieName, previousScreen, null, new string[5], null, 5, Date3, userClick);
             }
+           
 
         }
     }
@@ -1193,7 +1199,7 @@ namespace Project_B
                         Console.WriteLine("Transaction completed. \n Thank you for you payment.");
                         Console.Beep();
                         userClick.AddInput("Bought With Google Pay => ");
-                        IncrDailyRevenue(10, userClick);                      
+                        IncrDailyRevenue(10, userClick);
                     }
                     else
                     {
@@ -1213,6 +1219,11 @@ namespace Project_B
                     Console.WriteLine("you did not press enter :/");
                     Payment(userClick);
                 }
+            }
+            else
+            {
+                Console.Clear();
+                Payment(userClick);
             }
         }
 
@@ -1243,6 +1254,11 @@ namespace Project_B
                     {
                         userClick.AddInput("Customer Menu => ");
                         MainMenu.CustomerMenu(userClick);
+                        break;
+                    }
+                default:
+                    {
+                        Offers(userClick);
                         break;
                     }
             }
@@ -1278,6 +1294,11 @@ namespace Project_B
                             MainMenu.Employee(userClick);
                             break;
                         }
+                    }
+                default:
+                    {
+                        ShowDailyRevenue(userClick);
+                        break;
                     }
             }
         }
